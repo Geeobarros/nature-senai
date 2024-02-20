846b5886d3d67869c5be860a5a87dd2f5709a7ce
Geeobarros <ge.barros96@gmail.com>
2024-02-20 19:51:02 -0300

    add M1S05

---
+++ Semana-5/array_frutas.js
@@ -0,0 +1,19 @@
+// const frutas = ['manga', 'uvas','pêssego']
+
+// console.log(frutas[2])
+
+
+
+
+const prompt = require('prompt-sync')()
+let frutas1 = []
+
+for(i = 0; i < 3; i++){
+    
+    const add_fruta = prompt('Digite uma fruta: ')
+
+    frutas1.push(add_fruta)
+}
+
+
+console.log(frutas1[2])
