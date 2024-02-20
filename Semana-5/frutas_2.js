9c538c5e4a9ef410e719063cb33bc671fd74cd1d
Geeobarros <ge.barros96@gmail.com>
2024-02-20 19:59:26 -0300

    add ex.2 M1S05

---
+++ Semana-5/frutas_2.js
@@ -0,0 +1,7 @@
+const frutas = ['manga', 'uvas','pêssego']
+
+frutas.push('Melancia') //ADICIONA UM ITEM AO FINAL DO ARRAY
+
+frutas.shift()  //REMOVE O PRIMEIRO ITEM DO ARRAY E DESLOCA OS OUTROS PARA A ESQUERDA
+
+console.log(frutas)
