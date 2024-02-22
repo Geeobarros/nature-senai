cd68266e32d696e25143c1435c35adcfac0960cd
Geeobarros <ge.barros96@gmail.com>
2024-02-22 15:11:23 -0300

    add array map

---
+++ Semana-5/array-map.js
@@ -0,0 +1,10 @@
+/*
+Utilizando o array numeros utilizado nos exercícios anteriores, crie um novo array chamado quadrados que contenha o quadrado de cada número do array numeros. Imprima o array quadrados no console. Use o método map para realizar a criação de um novo array.
+ */
+const numeros = [7, 30, 25, 20, 50]
+
+const quadrado = numeros.map(q => {
+    return q **2
+})
+
+console.log(quadrado)
