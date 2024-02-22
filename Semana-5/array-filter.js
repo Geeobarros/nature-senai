e5f3a5c00617afc702bf4ff150f2e0a7023e6dcc
Geeobarros <ge.barros96@gmail.com>
2024-02-22 15:03:33 -0300

    add array filter

---
+++ Semana-5/array-filter.js
@@ -0,0 +1,12 @@
+/*
+Crie um array chamado pares que contenha apenas os números pares do array numeros criado nos exercícios anteriores. Em seguida, imprima esse novo array no console. Utilize o método filter para realizar a operação de filtragem.
+*/
+const numeros = [7, 30, 25, 20, 50]
+
+const pares  = numeros.filter(n => {
+    if (n % 2 == 0){
+        return true
+    }
+})
+
+console.log(pares)
