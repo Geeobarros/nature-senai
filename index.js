e8d8f4609d010addcd379f10fbcd4f84e15164de
Geeobarros <ge.barros96@gmail.com>
2024-03-28 13:13:10 -0300

    add exercicios express

---
+++ index.js
@@ -0,0 +1,10 @@
+// //ex1. Crie um novo projeto Node.js.
+// Instale o Express.js usando o npm.
+// Crie um arquivo app.js e configure um servidor Express básico que escute na porta 3000.
+
+const express = require("express")
+const app = express();
+
+app.listen(3000, () =>{
+    console.log("Servidor rodando")
+})
